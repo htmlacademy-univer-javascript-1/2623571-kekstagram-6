a1a6dd1d0143dc1557829e8a6f7335bf8d0cfe9a
Михаил Будущев <mbudushhev@bk.ru>
2025-10-17 20:51:47 +0500

    commit

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,25 @@ isPalindrome('ДовОд');
 isPalindrome('Кекс');
 
 checkStringLength('Лёша на полке клопа нашёл ');
+
+
+function isMeetingWithinWorkday(workStart, workEnd, meetingStart, meetingDuration) {
+  // Функция для преобразования времени в минуты
+  function timeToMinutes(timeStr) {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  const workStartMinutes = timeToMinutes(workStart);
+  const workEndMinutes = timeToMinutes(workEnd);
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
+}
+
+isMeetingWithinWorkday('08:00', '17:30', '14:00', 90);
+isMeetingWithinWorkday('8:0', '10:0', '8:0', 120);
+isMeetingWithinWorkday('08:00', '14:30', '14:00', 90);
+isMeetingWithinWorkday('14:00', '17:30', '08:0', 90);
+isMeetingWithinWorkday('8:00', '17:30', '08:00', 900);
